cb8824d4f81e63ba9f9bea13422eba6ae282277e
stanlocktop <wlad-test@ukr.net>
2023-09-10 12:40:11 +0300

    Finish Task1

--- Lesson_4/Task1/Program.cs
+++ Lesson_4/Task1/Program.cs
@@ -1,21 +1,29 @@
 ﻿namespace Task1
 {
-//    1) Написати програму, яка перевіряє введене число на парність/непарність.
-
-//13 -> "непарне"
-
-//22 -> "парне"
+    //    1) Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений. Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
     class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Введіть число: ");
-            int a = Convert.ToInt32(Console.Read());
+            string truePassword = "root";
+            string enteredPassword;
+            do
             {
-                if (a % 2 == 0) Console.WriteLine("Парне");
-                else Console.WriteLine("Непарне");
-            }
+                Console.ForegroundColor = ConsoleColor.White; // устанавливаем цвет
+                Console.Write("Введіть пароль: ");
+                Console.ResetColor(); // сбрасываем в стандартный
+                enteredPassword = Console.ReadLine();
+                if (enteredPassword != truePassword)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+                    Console.WriteLine("Неправильний пароль!");
+                    Console.ResetColor(); // сбрасываем в стандартный
+                }
+            } while (enteredPassword != truePassword);
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine("Вхід дозволено :) Ви увійшли в систему :)");
+            Console.ResetColor(); // сбрасываем в стандартный
             Console.ReadKey();
         }
     }
